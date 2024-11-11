b6ffb92095ddb02426a77d62b5b90c0d603d5f24
zmwebdev <zmwebdev@gmail.com>
2024-11-11 13:08:08 +0100

    Update Herria.php

--- eguraldia/MVC_eguraldia/model/Herria.php
+++ eguraldia/MVC_eguraldia/model/Herria.php
@@ -1,1 +1,20 @@
 <?php
+
+namespace Models;
+
+//use PDO;
+use Config\Database;
+
+class Herria {
+    private $db;
+        
+    public function __construct() {
+        $this->db = Database::getInstance()->getConnection();
+    }
+
+    public function getAll() {
+        $stmt = $this->db->prepare("SELECT * FROM herria ORDER BY izena");
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }    
+}
