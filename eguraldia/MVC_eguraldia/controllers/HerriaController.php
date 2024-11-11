31dff44130fe0ffada382b550d236d29f61f2fec
zmwebdev <zmwebdev@gmail.com>
2024-11-11 13:08:28 +0100

    Update HerriaController.php

--- eguraldia/MVC_eguraldia/controllers/HerriaController.php
+++ eguraldia/MVC_eguraldia/controllers/HerriaController.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace Controllers;
+
+use Models\Herria;
+
+class HerriaController {
+
+    public function listAll(){
+        $herria = new Herria();
+        $herriak = $herria->getAll();
+        //echo "<pre>"; var_dump($herriak);echo "</pre>";
+
+        require_once '../views/herri-zerrenda.php';
+    }
+}
