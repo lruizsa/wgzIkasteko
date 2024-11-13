bb8ddea004d0d7a26be5e65ca98f8c40408259da
zmwebdev <zmwebdev@gmail.com>
2024-11-13 10:57:11 +0100

    Update HerriaController.php

--- eguraldia/MVC_eguraldia/controllers/HerriaController.php
+++ eguraldia/MVC_eguraldia/controllers/HerriaController.php
@@ -10,8 +10,8 @@ class HerriaController {
         $herria = new Herria();
         $herriak = $herria->getAll();
         //echo "<pre>"; var_dump($herriak);echo "</pre>";
-        //require_once '../views/herri-zerrenda.php';
-        require_once '../views/herri-zerrenda-iragarpen-egunak-lortzeko.php';
+        require_once '../views/herri-zerrenda.php';
+        //require_once '../views/herri-zerrenda-iragarpen-egunak-lortzeko.php';
     }
 
     public function kudeatu(){
@@ -21,5 +21,25 @@ class HerriaController {
         //require_once '../views/herri-zerrenda.php';
         require_once '../views/herrien-kudeaketa.php';
     }
+
+    public function herriaGehitu($izena){
+        //var_dump($izena);
+        $herria = new Herria();
+        $herria->create($izena);
+        $herriak = $herria->getAll();
+        //echo "<pre>"; var_dump($herriak);echo "</pre>";
+        //require_once '../views/herri-zerrenda.php';
+        require_once '../views/herrien-kudeaketa.php';
+    }
+
+    public function herriaEzabatu($id){
+        //var_dump($izena);
+        $herria = new Herria();
+        $herria->delete($id);
+        $herriak = $herria->getAll();
+        //echo "<pre>"; var_dump($herriak);echo "</pre>";
+        //require_once '../views/herri-zerrenda.php';
+        require_once '../views/herrien-kudeaketa.php';
+    }    
 
 }
