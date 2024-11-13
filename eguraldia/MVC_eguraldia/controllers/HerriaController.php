6a4d1895121fa57e84f2c95c742168e5e30b2bc0
zmwebdev <zmwebdev@gmail.com>
2024-11-13 09:01:55 +0100

    Update HerriaController.php

--- eguraldia/MVC_eguraldia/controllers/HerriaController.php
+++ eguraldia/MVC_eguraldia/controllers/HerriaController.php
@@ -10,7 +10,16 @@ class HerriaController {
         $herria = new Herria();
         $herriak = $herria->getAll();
         //echo "<pre>"; var_dump($herriak);echo "</pre>";
-
-        require_once '../views/herri-zerrenda.php';
+        //require_once '../views/herri-zerrenda.php';
+        require_once '../views/herri-zerrenda-iragarpen-egunak-lortzeko.php';
     }
+
+    public function kudeatu(){
+        $herria = new Herria();
+        $herriak = $herria->getAll();
+        //echo "<pre>"; var_dump($herriak);echo "</pre>";
+        //require_once '../views/herri-zerrenda.php';
+        require_once '../views/herrien-kudeaketa.php';
+    }
+
 }
