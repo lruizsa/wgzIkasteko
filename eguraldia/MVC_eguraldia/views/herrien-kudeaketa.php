05134c46d9e3eafd179aa7c1f6872edbf1c24f6c
zmwebdev <zmwebdev@gmail.com>
2024-11-13 11:00:23 +0100

    Update herrien-kudeaketa.php

--- eguraldia/MVC_eguraldia/views/herrien-kudeaketa.php
+++ eguraldia/MVC_eguraldia/views/herrien-kudeaketa.php
@@ -7,16 +7,19 @@
 </head>
 <body>
     <h1>Herri zerrenda</h1>
-    <form action="iragarpenak.php" method="GET">
+    <form action="" method="GET">
         <?php foreach ($herriak as $herria): ?>
             <label>
                 <input type="radio" name="herria_id" value="<?= $herria['id'] ?>" required>
                 <?= htmlspecialchars($herria['izena']) ?>
             </label><br>
         <?php endforeach; ?>
-        <button type="submit">Gehitu</button>
         <button type="submit">Ezabatu</button>
         <button type="submit">Aldatu</button>
+        <br>
+    </form>
+    <form action="herria-gehitu.php">
+        <button type="submit">Gehitu</button>
     </form>
 </body>
 </html>
